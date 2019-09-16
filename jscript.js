dc1e86a369bbaea5bab8af5f65b69ac5e920d553
Kishore Devaraj <kishore.devaraj@reedexpo.co.uk>
2019-09-16 21:56:23 +0530

    Get form values in js

--- jscript.js
+++ jscript.js
@@ -1,21 +1,6 @@
 function check(form) {
-    if(form.userid.value =="rohini" && form.pswrd.value =="12345")
-    {
-        window.open('https://github.com/Rohini-Sampath/roh-sam.git')
-    }
-    else
-    {
-        alert("The username and password doesn't match")
-    }
-console.log(userid).value;
-console.log(pswrd).value;
+    var nameValue = document.getElementById("userid").value;
+    console.log(nameValue)
+    // console.log(form.userid.value)
+    // console.log(form.passwd.value)
 }
-
-// function getInputValue(){
-//     // Selecting the input element and get its value 
-//     var inputVal = console.log("userid").value;
-//     var inputVal = console.log("pswrd").value;
-    
-//     // Displaying the value
-//     alert(inputVal);
-// }
